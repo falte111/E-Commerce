a413d16e84c272b49a9ee1d7292ef8d4c0567386
Jian Zhong <60372266+falte111@users.noreply.github.com>
2024-02-13 22:52:20 +0800

    update background images used

--- app/src/Home.js
+++ app/src/Home.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Navbar from "./Navbar";
 import background from "./resource/musicbg.jpg";
 import background2 from "./resource/pianoplaying.jpg"
-import background3 from "./resource/pianobook.jpg"
+import background3 from "./resource/trumpet.jpg"
 import { Grid } from "@mui/material";
 import React, { useState, useEffect } from 'react';
 import './BackgroundChanger.css'; 
@@ -57,8 +57,8 @@ function Home() {
         justifyContent: 'center',
         height: '100vh',}}>
         <div>
-        <h1 style={{fontSize:"80px", margin:"0px" , padding:"0px", fontFamily:"sans-serif"}}>Music is your world</h1>
-        <h3 style = {{paddingLeft:"35%", fontFamily:"sans-serif"}}>Dont miss a chance!</h3>
+        <h1 style={{fontSize:"80px", margin:"0px" , padding:"0px", fontFamily:"sans-serif", color: "white"}}>Music is your world</h1>
+        <h3 style = {{paddingLeft:"35%", fontFamily:"sans-serif", color: "white"}}>Dont miss a chance!</h3>
         <div style={{paddingLeft:"35%"}}><button style={buttonStyle}><span style={{color:"#4f504b", fontFamily:"Droid Sans"}}>START LEARNING</span></button></div>
         {/* onMouseEnter={} */}
         </div>
