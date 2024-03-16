8caba64c4594f71c04352bb02459bdae3192ab24
Jian Zhong <60372266+falte111@users.noreply.github.com>
2024-03-16 15:28:51 +0800

    add academy content section with hover effect

--- app/src/Home.js
+++ app/src/Home.js
@@ -13,6 +13,7 @@ import whatsapplogo from "./resource/WhatsAppButtonGreenSmall.svg"
 import { Grid } from "@mui/material";
 import React, { useState, useEffect } from 'react';
 import './BackgroundChanger.css'; 
+import './imagehover.css'
 import Divider from '@mui/material/Divider';
 
 function Home() {
@@ -68,8 +69,8 @@ function Home() {
         {/* onMouseEnter={} */}
         </div>
         </div>
-        <a aria-label="Chat on WhatsApp" href="https://wa.me/+6591124691?text=Hi%2C%20I%20am%20interested%20in%20the%20free%20trial%20lesson%20by%20Quindicesima%21"> <img alt="Chat on WhatsApp" src={whatsapplogo} style={{position:"fixed", bottom:0,right:0}} /></a>
       </Grid>
+      <a aria-label="Chat on WhatsApp" href="https://wa.me/+6591124691?text=Hi%2C%20I%20am%20interested%20in%20the%20free%20trial%20lesson%20by%20Quindicesima%21"> <img alt="Chat on WhatsApp" src={whatsapplogo} style={{position:"fixed", bottom:0,right:0}} /></a>
       {/* Line break by adding br tag to separate background and contents */}
       <br/> 
       <Divider/>
@@ -78,10 +79,15 @@ function Home() {
   alignItems="center"
   justifyContent="center">
         <Grid item>
-          <div>
-          <h1>ACADEMY</h1>
+          <div className="container">
+          <h1 style={{textAlign:"center"}}>ACADEMY</h1>
           {/* https://stackoverflow.com/questions/66226603/how-to-fit-images-to-different-screen-resolutions */}
-          <img src={classroom} style={{maxWidth:"100%"}}></img>
+          <a href="http://localhost:3000/courses">
+          <img className="image" src={classroom} style={{maxwidth:"100%" , height:"600px"}}></img>
+          <div class="middle">
+    <div class="text">FIND OUT MORE!</div>
+  </div>
+  </a>
           </div>
         
         </Grid>
